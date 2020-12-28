e6995eade1138eeb6b9f807dd20d038b9c9bc02f
Yuriy Dmitriev <it@r117.com>
2020-12-28 22:37:05 +0200

    Reformat code.

--- src/RunAsyncWorker.cc
+++ src/RunAsyncWorker.cc
@@ -4,33 +4,25 @@
 Value runAsyncWorker(const CallbackInfo& info) {
     Napi::Env env = info.Env();
     uint32_t pcount =  info.Length();
-
     if(pcount < 2) {
         Napi::TypeError::New(env, "Expected minimum 2 args. (cb, 'sql') arguments.")
                 .ThrowAsJavaScriptException();
         return env.Null();
     }
-
     Function callback = info[0].As<Function>();
     std::string sql = info[1].As<Napi::String>().Utf8Value();
-
     pqAsyncWorker* asyncWorker;
     std::vector<std::string> vparams;
-
     if(pcount == 2){
         // simple mode
          asyncWorker = new pqAsyncWorker(callback, sql);
-
     } else{
-
-
         for (uint32_t i = 2; i < pcount; ++i) {
             std::string s = info[i].As<Napi::String>().Utf8Value();
             vparams.push_back(s);
         }
         asyncWorker = new pqAsyncWorker(callback, sql, vparams);
     }
-
     asyncWorker->Queue();
     std::string msg =  "pqAsyncWorker  queued.";
     return String::New(env, msg.c_str());
